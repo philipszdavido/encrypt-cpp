96b9e662e62f6c26c0ee71dcd94851d5e849b0d0
Chidume Nnamdi <kurtwanger40@gmail.com>
2025-08-05 18:28:24 +0100

    Initial Commit

---
+++ encrypt/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  encrypt
+//
+//  Created by Chidume Nnamdi on 05/08/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
