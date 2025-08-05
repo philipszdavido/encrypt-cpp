75099c0f3d7e91d07a212ce2df30dffa0c085a2c
Chidume Nnamdi <kurtwanger40@gmail.com>
2025-08-05 23:12:07 +0100

    fixed encryption to add signature header

--- encrypt/main.cpp
+++ encrypt/main.cpp
@@ -11,31 +11,49 @@
 using namespace std;
 
 string getFileExtension(string);
-void encrypt(string);
-void decrypt(string);
+void encrypt(string, const string&);
+void decrypt(string, const string&);
+vector<byte> readfile(const string&);
+string signature = "NNAMDI_CHIDUME_SIGN";
+const char key = 0xAA;
 
 int main(int argc, const char * argv[]) {
         
-    string filename = static_cast<string>(argv[1]);
-    string extension = getFileExtension(filename);
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <filename> <mode: 1 - encrypt, 2 - decrypt> <password>\n";
+        return 1;
+    }
     
+    string filename = static_cast<string>(argv[1]);
+    const char* mode = argv[2];
+    string password = static_cast<string>(argv[3]);
+    
+    string extension = getFileExtension(filename);
+        
     if (extension != "jpg") {
         cout << "Unsupported file type";
         return 1;
     }
     
-    const char * mode = argv[2];
+    if (password.length() == 0) {
+        cout << "There is no password.";
+        return 1;
+    }
     
-    cout << filename << "\n";
-    cout << mode << "\n";
-
+    if (!mode) {
+        cout << "Specify mode";
+        return 1;
+    }
+        
+    cout << "Filename: " << filename << "\n";
+    cout << "Mode: " << mode << " : " << (*mode == '1' ? "Encryption" : "Decryption") << "\n";
     
     if(strcmp(mode, "1") == 0) {
-        encrypt(filename);
+        encrypt(filename, password);
     }
     
     if(strcmp(mode, "2") == 0) {
-        decrypt(filename);
+        decrypt(filename, password);
     }
     
     return 0;
@@ -66,13 +84,12 @@ string getFileExtension(string name) {
     
 }
 
-void encrypt(string filename) {
+vector<byte> readfile(const string& filename) {
     
     std::ifstream in(filename, std::ios::binary);
     std::vector<std::byte> data;
     
     char c;
-    const char key = 0xAA;
     
     while (in.get(c)) {
         data.push_back(static_cast<byte>(c));
@@ -80,20 +97,75 @@ void encrypt(string filename) {
     
     in.close();
     
-    std::ofstream out(filename,
-                      std::ios::binary);
+    return data;
+
+}
+
+void encrypt(string filename, const string& password) {
+    
+    vector<byte> data = readfile(filename);
+    
+    std::ofstream out(filename, std::ios::binary);
+    
+    // add file sign
+    for (int i = 0; i < signature.length(); ++i) {
+        out.put(signature[i]);
+    }
 
     for (int i = 0; i < data.size(); ++i) {
-        const byte v = data[i] ^ static_cast<byte>(key);
-        out.put((char)v);
+        
+        byte x = static_cast<byte>(password[i % password.size()]) ^ static_cast<byte>(key);
+        
+        const byte xored = data[i] ^ x;
+        
+        out.put((char)xored);
+        
     }
     
     out.close();
 
 }
 
-void decrypt(string filename) {
+void decrypt(string filename, const string& password) {
+
+    vector<byte> data = readfile(filename);
+
+    // check if signature is present
     
-    encrypt(filename);
+    size_t len = signature.length();
+    string sig;
+    
+    for (int i = 0; i < len; ++i) {
+        cout <<static_cast<char>(data[i]);
+        sig += static_cast<char>(data[i]);
+    }
+    cout << "\n";
+    
+    cout << sig << "\n";
+    cout << (sig == signature) << "\n" ;
+    
+    if (sig == signature) {
+
+        std::ofstream out(filename, std::ios::trunc | std::ios::binary);
+
+        // decrypt
+        for (int i = 0; i < data.size(); ++i) {
+                        
+            if (i < len) {
+                continue;
+            }
+            
+            byte x = static_cast<byte>(password[i % password.size()]) ^ static_cast<byte>(key);
+            
+            const byte xored = data[i] ^ x;
+            
+            out.put((char)xored);
+            
+        }
+        
+        out.close();
+
+    }
+
 
 }
